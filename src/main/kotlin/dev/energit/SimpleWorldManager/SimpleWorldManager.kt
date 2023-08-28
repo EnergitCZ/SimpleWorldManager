039f598bd115b4c42302ef46342d9d8bb4d63fdd
energit <energit@virtcz.eu>
2023-08-28 13:28:48 +0200

    Initial commit

---
+++ src/main/kotlin/dev/energit/SimpleWorldManager/SimpleWorldManager.kt
@@ -0,0 +1,9 @@
+package dev.energit.SimpleWorldManager
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class SimpleWorldManager : JavaPlugin() {
+    override fun onEnable() {
+
+    }
+}
---
+++ src/main/kotlin/dev/energit/SimpleWorldManager/SimpleWorldManagerApi.kt
@@ -0,0 +1,4 @@
+package dev.energit.SimpleWorldManager
+
+class SimpleWorldManagerApi {
+}
